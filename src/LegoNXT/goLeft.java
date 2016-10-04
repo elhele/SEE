f0e3fd7a2486292bfaa8355a622cf14f10d805c8
Evgeniya Ovchinnikova <evgeniya.ovchinnikova@inf.h-brs.de>
2016-10-04 16:36:03 +0200

    Update goLeft.java

--- src/LegoNXT/goLeft.java
+++ src/LegoNXT/goLeft.java
@@ -1,4 +1,4 @@
-package LegoNXT;
+package ljTest;
 
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
@@ -6,23 +6,25 @@ import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.MoveController;
 import lejos.util.Delay;
 import lejos.nxt.LightSensor;
+import lejos.nxt.Button;
 
 public class goLeft {
 
-        public static void main(String[] args) {
-                double arcRad = 30;
-                double angle = 90;
-                LightSensor lightSens1 = new LightSensor(SensorPort.S1);
-                LightSensor lightSens2 = new LightSensor(SensorPort.S2);
-                DifferentialPilot dp = new DifferentialPilot(MoveController.WHEEL_SIZE_NXT1, 18, Motor.B, Motor.C, true);
-
-                lightSens1.setHigh(100);
-                lightSens2.setHigh(100);
-                Delay.msDelay(5000);
-                dp.setTravelSpeed(10);
-                dp.arc(-arcRad, angle);
-                dp.stop();
-                Delay.msDelay(10000);
-        }
+	public static void main(String[] args) {
+		double arcRad = 40;
+		double angle = 90;
+		Button.LEFT.waitForPressAndRelease();
+		LightSensor lightSens1 = new LightSensor(SensorPort.S1);
+		LightSensor lightSens2 = new LightSensor(SensorPort.S2);
+		DifferentialPilot dp = new DifferentialPilot(MoveController.WHEEL_SIZE_NXT1, 18, Motor.B, Motor.C, true);
+		lightSens1.setHigh(100);
+		lightSens2.setHigh(100);
+		Button.RIGHT.waitForPressAndRelease();
+		Button.ESCAPE.waitForPressAndRelease();
+		dp.setTravelSpeed(10);
+		dp.arc(-arcRad, angle);
+		dp.stop();
+		Button.ESCAPE.waitForPressAndRelease();
+	}
 
 }
